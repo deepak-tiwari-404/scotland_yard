46110723b71c3ec6ca5166ea63b28c2b9637bbbc
Deepak Tiwari <deepak.tiwari.1493@gmail.com>
2015-06-01 00:37:33 +0530

    user signup and login

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,2 +1,39 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+
+var GameEngine = {
+  initialize: function(){
+    var canvas = document.getElementById("mazecanvas");
+    var context = canvas.getContext("2d");
+    var startX = 50;
+    var startY = 50;
+    var boardWidth = 500;
+    var boardHeight = 500;
+    var blockHeight = 50;
+    var blockWidth = 50;
+    var streetWidth = 5;
+    var streetColor = "#FFFFFF";
+    function drawMazeAndRectangle() {
+        makeWhite(0, 0, canvas.width, canvas.height);
+        for(var i=0; i <= boardWidth/blockWidth; i++){
+          drawRectangle((i+1)*blockWidth - streetWidth, startY, streetWidth, boardHeight, streetColor);
+          drawRectangle(startX, (i+1)*blockHeight - streetWidth, boardWidth, streetWidth, streetColor);
+        }
+    }
+    function drawRectangle(i, j, width, height, style) {
+        context.beginPath();
+        context.rect(i,j,width,height);
+        context.closePath();
+        context.fillStyle = style;
+        context.fill();
+    }
+    function makeWhite(x, y, w, h) {
+      context.beginPath();
+      context.rect(x, y, w, h);
+      context.closePath();
+      context.fillStyle = "#555555";
+      context.fill();
+    }
+    drawMazeAndRectangle();
+  } 
+};
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150530190753) do
+ActiveRecord::Schema.define(version: 20150531185203) do
 
   create_table "cities", force: :cascade do |t|
     t.string   "name"
@@ -52,6 +52,7 @@ ActiveRecord::Schema.define(version: 20150530190753) do
     t.string   "password_digest"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+    t.string   "remember_digest"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
